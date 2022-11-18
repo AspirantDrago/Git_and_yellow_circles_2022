40576dcc7e5f8b662e0f655f6142184ba5329c89
Александр <sanekclubstr@mail.ru>
2022-11-18 20:05:24 +0300

    Add UI.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
 import random
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
 
+from UI import Ui_Form
 
-class Example(QWidget):
-    CIRCLE_COLOR = QColor(255, 255, 0)
+
+class Example(QWidget, Ui_Form):
+
     MINIMAL_DIAMETR = 10
 
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.click)
         self.is_draw = False
 
@@ -29,7 +30,8 @@ class Example(QWidget):
 
     def draw_circle(self, qp):
         diameter = random.randint(self.MINIMAL_DIAMETR, min(self.width(), self.height()) // 2)
-        qp.setBrush(self.CIRCLE_COLOR)
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        qp.setBrush(color)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
         qp.drawEllipse(x, y, diameter, diameter)
